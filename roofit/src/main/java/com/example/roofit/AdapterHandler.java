6c53112735bf4cb7210a584b42a0498b7d82d028
kongsin <pansansou@gmail.com>
2016-08-21 04:39:32 +0700

    call with path

--- roofit/src/main/java/com/example/roofit/AdapterHandler.java
+++ roofit/src/main/java/com/example/roofit/AdapterHandler.java
@@ -8,7 +8,6 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by kongsin on 8/20/16.
@@ -25,23 +24,27 @@ public class AdapterHandler implements InvocationHandler {
     public Object invoke(Object proxy, Method m, Object[] args)
             throws Throwable {
 
-        Type returnType = m.getGenericReturnType();
-        String typeClass = returnType.toString().split("<")[1].split(">")[0];
-        Object returnObject;
+        Type resObjectClassName = m.getGenericReturnType();
+        String typeClass = resObjectClassName.toString().split("<")[1].split(">")[0];
+
         Constructor constructor = m.getReturnType().getDeclaredConstructor(String.class, String.class, Class.class);
         constructor.setAccessible(true);
-        String actualUrl = getAnnotationUrl(m);
+
+        return constructor.newInstance(queryPathParam(m, args), mBaseUrl, Class.forName(typeClass));
+    }
+
+    private String queryPathParam(Method m, Object[] args) {
+        String actualUrl = getRawUrl(m);
         ArrayList<Param> params = getPathAnnotation(m, args);
         if (actualUrl != null) {
             for (Param param : params) {
                 actualUrl = actualUrl.replace("{" + param.name + "}", param.value);
             }
         }
-        returnObject = constructor.newInstance(actualUrl, mBaseUrl, Class.forName(typeClass));
-        return returnObject;
+        return actualUrl;
     }
 
-    private String getAnnotationUrl(Method m) {
+    private String getRawUrl(Method m) {
         Annotation[] annotations = m.getAnnotations();
         for (Annotation annotation : annotations) {
             if (annotation instanceof GET) {
